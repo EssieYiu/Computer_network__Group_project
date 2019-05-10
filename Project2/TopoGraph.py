ba8d73b05799b1a02e7f0a9d9cc5a082f0a70f7e
galaxyzzang <49181335+galaxyzzang@users.noreply.github.com>
2019-05-10 19:45:34 +0800

    Update TopoGraph.py

--- Project2/TopoGraph.py
+++ Project2/TopoGraph.py
@@ -6,10 +6,23 @@ class TopoGraph:
         self.name_ip={}
         #边和cost的对应关系
         self.link_cost={}
-        #node和其所有邻居的对应关系
-        self.edge={}
+
     #获取某个节点的所有邻居以及与邻居的linkcost
     def get_allLink(self,name):
+        ip=name_ip[name]
+        allLink={}  #节点->cost
+        for link,cost in self.link_cost.items():
+            if ip in link:
+                n=(link[0] if link[0]!=ip else link[1])
+                allLink[n]=cost
+        return allLink
+
+    def change_linkCost(self,A,B,new_cost):
+        ipA=name_ip[A]
+        ipB=name_ip[B]
+        self.link_cost
+
+
 
 
 
