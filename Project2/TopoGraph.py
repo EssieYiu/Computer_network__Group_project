b5564d27de4a6654ae8047dd30500966238482c0
EssieYiu <31929622+EssieYiu@users.noreply.github.com>
2019-05-10 20:26:47 +0800

    update toplogy
    update

--- Project2/TopoGraph.py
+++ Project2/TopoGraph.py
@@ -1,15 +1,9 @@
 import random
-<<<<<<< HEAD
-IP1 = ""
 IP2 = ""
 IP3 = ""
 IP4 = ""
 IP5 = ""
-=======
->>>>>>> 74bffd5357d617ef30fbde39110e63fa9d7c9fbc
 class TopoGraph:
-    def __init__(self,n,e):
-        self.N=n
         self.E=e
         #名字和ip的对应关系
         self.name_ip={}
@@ -18,11 +12,7 @@ class TopoGraph:
 
     #获取某个节点的所有邻居以及与邻居的linkcost
     def get_allLink(self,name):
-<<<<<<< HEAD
         ip=self.name_ip[name]
-=======
-        my_ip=name_ip[name]
->>>>>>> 74bffd5357d617ef30fbde39110e63fa9d7c9fbc
         allLink={}  #节点->cost
         for link,cost in self.link_cost.items():
             if my_ip in link:
@@ -30,7 +20,6 @@ class TopoGraph:
                 allLink[neigh_ip]=cost
         return allLink
 
-<<<<<<< HEAD
     def initialize_graph(self):
         self.link_cost[(IP1,IP3)] = 34
         self.link_cost[(IP1,IP5)] = 34
@@ -50,10 +39,3 @@ class TopoGraph:
 
 
 
-=======
-    def change_linkCost(self):
-        for link in self.link_cost.keys():
-            new_cost=random.randint(1,50)
-            self.link_cost[link]=new_cost
-        print("* Link cost has changed!")
->>>>>>> 74bffd5357d617ef30fbde39110e63fa9d7c9fbc
