513bdd94df715fb156cffa9c2ea5106a2c8203d3
galaxyzzang <49181335+galaxyzzang@users.noreply.github.com>
2019-05-16 11:14:52 +0800

    Update TopoGraph.py

--- Project2/TopoGraph.py
+++ Project2/TopoGraph.py
@@ -5,17 +5,17 @@ IP3 = ""
 IP4 = ""
 IP5 = ""
 class TopoGraph(object):
-    def __init__(self,e,n):
-        self.N = n
-        self.E=e
+    def __init__(self):
+        #self.N = n
+        #self.E=e
         #名字和ip的对应关系
-        self.name_ip={}
+        self.name_ip={'A':IP1,'B':IP2,'C':IP3,'D':IP4,'E':IP5}
         #边和cost的对应关系
         self.link_cost={}
         self.ip_changeable_route = {} #ip->list[nei1,nei2...],IP地址到list的映射关系，其中list存储邻居ip，表示它能修改到此邻居的路径权重
 
     #获取某个节点的所有邻居以及与邻居的linkcost
-    def get_allLink(self,name):
+    def get_allNeighbour(self,name):
         my_ip=self.name_ip[name]
         allLink={}  #节点->cost
         for link,cost in self.link_cost.items():
@@ -40,3 +40,8 @@ class TopoGraph(object):
     def node_changeable_route(self,node_ip):
         return self.ip_changeable_route[node_ip]
 
+    def get_down(self,name):
+        if name=='A':
+            return True
+        else:
+            return False
