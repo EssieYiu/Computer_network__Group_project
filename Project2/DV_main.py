dee23255c500780cace9e8a75e75ce50140a8b07
galaxyzzang <49181335+galaxyzzang@users.noreply.github.com>
2019-05-17 09:45:52 +0800

    Update DV_main.py

--- Project2/DV_main.py
+++ Project2/DV_main.py
@@ -13,7 +13,6 @@ def change_period(node):
         
     #my_ip = input('Please enter your ip address')
     #my_name = input('Please enter your name')
-    
 my_ip="172.19.112.247"
 my_name='A'
 Graph = TopoGraph()
@@ -25,23 +24,33 @@ my_node = Node(my_name,my_ip,neighbour,down)
     #线程共用的：my_node的sck_output
     #1.周期性发送DV消息
     #send_DV:sendto
+my_thread=[]
 DVthread=threading.Thread(target=sendDV_period,args=(my_node,))
-DVthread.start()
+my_thread.append(DVthread)
+#DVthread.start()
 
     #2.接收
     #涉及帮忙转发:sendto
 Rthread=threading.Thread(target=my_node.recv,args=())
-Rthread.start()
+my_thread.append(Rthread)
+#Rthread.start()
 
     #3.发送消息
     #send_message:sendto
 Sthread=threading.Thread(target=my_node.send_message,args=())
-Sthread.start()
+my_thread.append(Sthread)
+#Sthread.start()
    
     #4.周期性更改链路代价
     #change_route:sendto
 Cthread=threading.Thread(target=change_period,args=(my_node,))
-Cthread.start()
+my_thread.append(Cthread)
+#Cthread.start()
+for thread in my_thread:
+    thread.start()
+
+for thread in my_thread:
+    thread.join()
 
 
 
