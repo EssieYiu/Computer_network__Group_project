1cc4788257b3659fa64dec4bb05fcb8fc81322ff
galaxyzzang <49181335+galaxyzzang@users.noreply.github.com>
2019-05-17 22:21:03 +0800

    Update DV_main.py

--- Project2/DV_main.py
+++ Project2/DV_main.py
@@ -5,23 +5,32 @@ import time
 def sendDV_period(node):
     while 1:
         node.send_DV()
-        print(node.table)
+        print("DV:",node.DV)
+        print("table:",node.table)
+        print()
         time.sleep(10)
 def change_period(node):
     while 1:
-        time.sleep(20)
+        time.sleep(30)
         node.change_route()
-        print("after change:",node.neighbour)
+        print("after change neighbour:",node.neighbour)
+        print("DV:",node.DV)
 def sendMessage(node):
     while 1:
         node.send_message()
 def recv(node):
     while 1:
         node.recv()
+def downAndrecover(node):
+    time.sleep(60)
+    if node.down==True:
+        node.go_down()
+        time.sleep(60)
+        node.recover()
     #my_ip = input('Please enter your ip address')
     #my_name = input('Please enter your name')
-my_ip="192.168.199.205"
-my_name='D'
+my_ip="192.168.199.131"
+my_name='A'
 Graph = TopoGraph()
 Graph.initialize_graph()
 neighbour=Graph.get_allNeighbour(my_name)
@@ -29,9 +38,8 @@ down=Graph.get_down(my_name)
 changeable=Graph.node_changeable_route(my_ip)
 print("changeable route:",changeable)
 my_node = Node(my_name,my_ip,neighbour,changeable,down)
-my_node.neighbour={'192.168.199.131':34}
+my_node.neighbour={'192.168.199.102':34,'192.168.199.205':34}
 my_node.DV['127.0.0.1']=1000000
-my_node.DV['127.0.0.2']=1000000
 my_thread=[]
     #线程共用的：my_node的sck_output
 
@@ -58,8 +66,11 @@ my_thread.append(Sthread)
 Cthread=threading.Thread(target=change_period,args=(my_node,))
 my_thread.append(Cthread)
 #Cthread.start()
+
+Dthread=threading.Thread(target=downAndrecover,args=(my_node,))
+my_thread.append(Dthread)
 for thread in my_thread:
-    time.sleep(3)
+    time.sleep(1)
     thread.start()
 
 for thread in my_thread:
@@ -70,18 +81,6 @@ for thread in my_thread:
     
 
 
-
-    #my_node.DV_neighbour=[{},{my_ip:10,'E':1},{},{},{my_ip:2,'B':1}]
-    
-    #my_node.initial_DV()
-    #print(my_node.neighbour)
-    #print(my_node.DV_neighbour)
-    #print(my_node.DV)
-    #print(my_node.table)
-    #my_node.recompute_DV()
-    #my_node.send_DV()
-    #my_node.recv()
-   # my_node.recompute_DV()
 
 
     
