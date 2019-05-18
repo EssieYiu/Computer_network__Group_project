c3c3c96dacce0dd2441ce5b37cd632f6da309cbb
EssieYiu <31929622+EssieYiu@users.noreply.github.com>
2019-05-18 13:21:58 +0800

    LS first publish
    LS first publish

---
+++ Project2/LS/LS_main.py
@@ -0,0 +1,5 @@
+from router import router
+from LSGraph import LSGraph
+
+if __name__ == "__main__":
+    pass
---
+++ Project2/LS/router.py
@@ -0,0 +1,146 @@
+import socket
+import random
+RECVPORT = 20000
+SENDPORT = 30000
+INF = 10000000
+BUFFSIZE = 4096
+TTL = 2
+NAMELIST = ['A','B','C','D','E']
+class router:
+    def __init__(self,name = 'A',ip = "0.0.0.0",neigh = {},name_ip = {}):
+        self.name_to_ip = name_ip #dict name->ip, neibour info,their name and address
+        self.neighbour = neigh #dict neighbour_name->cost
+        
+        self.topo = [[]] #adjacent matrix, store the map
+
+        self.next_jump = {} #dict dst_name->next jump
+        self.cost = {} #dict dst_name->cost
+
+        self.sck_input = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.sck_input.bind((ip,RECVPORT))
+        self.sck_output = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.sck_output.bind((ip,SENDPORT))
+
+        self.name = name
+        self.ip = ip
+
+
+    #use the exsiting topo graph to compute next_jump and cost
+    #dijkstra
+    def LS_algorithm(self):
+        dist = {} #dict node->distance from s,include myself
+        prev = {} #dict node->previous node,not include myself
+        for node in NAMELIST:
+            dist[node] = INF
+        dist[self.name] = 0
+        queue = ['A','B','C','D','E']
+        while len(queue):
+            #deletemin
+            cur_node = queue[0]
+            for n in queue:
+                if dist[n] < dist[cur_node]:
+                    cur_node = n
+            queue.remove(cur_node)
+            for i in range(5):
+                if dist[chr(i+ord('A'))] > dist[cur_node] + self.topo[ord(cur_node)-ord('A')][i]:
+                    dist[chr(i+ord('A'))] = dist[cur_node] + self.topo[ord(cur_node)-ord('A')][i]
+                    prev[chr(i+ord('A'))] = cur_node    
+        for node in NAMELIST:
+            if node != self.name:
+                self.cost[node] = dist[node]
+                next_temp = node
+                while self.neighbour.get(next_temp,0) == 0:
+                    next_temp = prev[next_temp]
+                self.next_jump[node] = next_temp
+
+    def handle_receive(self):
+        data,(fhost,fport) = self.sck_input.recvfrom(BUFFSIZE)
+        message = data.decode()
+        message = message.split(' ',3)
+        #meaningful message, decide whether to send or print
+        if message[0] == 0:
+            if message[2] == self.ip:
+                print("Receive message from",message[1],'debug:',fhost)
+                print(message[3])
+            else:
+                print("Help forward message from",message[1],"to",message[2])
+                dst = message[2]
+                self.sck_output.sendto(data,(dst,RECVPORT))
+        #broadcast route weight, change topo and neighbour
+        elif message[0] == 1:
+            host1 = message[1]
+            host2 = message[2]
+            weight = int (message[3])
+            self.topo[ord(host1)-ord('A')][ord(host2)-ord('A')] = weight
+            #only receive neibour down or recover info will enter this if
+            if host1 == self.ip:
+                self.neighbour[host1] = weight
+                print('one of my neibour down/recover,my route to it now:',weight)
+            #forward out, send to all its neighbour,with TTL -1
+            if int(message[4]) > 0:
+                message[4] = message[4] - 1
+                message = ' '.join(message)
+                for node in NAMELIST:
+                    if self.neighbour.get(node,0):
+                        next_stop = self.next_jump[node]
+                        next_stop_ip = self.name_to_ip[next_stop]
+                        self.sck_output(message.encode(),(next_stop_ip,RECVPORT))
+
+
+    def send_meaningful_message(self):
+        message = input("Please enter your message to send")
+        dst = input("Please enter the destination(A/B/C/D/E),but not yourself:")
+        if dst not in NAMELIST:
+            print("Invalid destionation!")
+        elif dst == self.name:
+            print("You should not send message to yourself")
+        else:
+            pack_msg = '0 '+self.ip+' '+self.name_to_ip(dst)+' '+message
+            next_stop = self.next_jump[dst]
+            if self.cost[dst] >= INF:
+                print("Send message failed,destination can not reach")
+            else:
+                self.sck_output.sendto(pack_msg.encode(),(self.name_to_ip(next_stop),RECVPORT))
+                print("Sending message to",dst)
+                print("Firstly send to",next_stop)
+
+    #broadcast all the linking route,first send to its neibour
+    def broadcast(self):
+        for node in NAMELIST:
+            if self.neighbour.get(node,0):
+                route_info = '1 '+self.name+' '+node+' '+str(self.cost[node])+' '+TTL
+                self.sck_output.sendto(route_info.encode(),(self.name_to_ip(node),RECVPORT))
+        print("Broadcast my route info")
+
+    #only change its own topo and neighbour info,
+    #can use broadcast to tell others about change
+    def change_route(self):
+        for node in NAMELIST:
+            if self.neighbour.get(node,0):
+                new_weight = random.randint(0,50)
+                self.neighbour[node] = new_weight
+                self.topo[ord(self.name)-ord('A')][ord(node)-ord('A')] = new_weight
+
+    #notice that once if down, can not change route anymore
+    def down(self):
+        for node in NAMELIST:
+            if self.neighbour.get(node,0):
+                self.neighbour[node] = INF
+                self.topo[ord(node)-ord('A')][ord(self.name)-ord('A')] = INF
+                self.topo[ord(self.name)-ord('A')][ord(node)-ord('A')] = INF
+                route_info = '1 '+self.name+' '+node+' '+str(INF)+' '+TTL
+                self.sck_output.sendto(route_info.encode(),(self.name_to_ip(node),RECVPORT))
+                route_info = '1 '+node+' '+self.name+' '+str(INF)+' '+TTL
+                self.sck_output.sendto(route_info.encode(),(self.name_to_ip(node),RECVPORT))
+
+    #recover two edges at the same time
+    def recover(self):
+        for node in NAMELIST:
+            if self.neighbour.get(node,0):
+                recover_weight = random.randint(0,50)
+                self.neighbour[node] = recover_weight
+                self.topo[ord(node)-ord('A')][ord(self.name)-ord('A')] = recover_weight
+                self.topo[ord(self.name)-ord('A')][ord(node)-ord('A')] = recover_weight
+                route_info = '1 '+self.name+' '+node+' '+str(recover_weight)+' '+TTL
+                self.sck_output.sendto(route_info.encode(),(self.name_to_ip(node),RECVPORT))
+                route_info = '1 '+node+' '+self.name+' '+str(recover_weight)+' '+TTL
