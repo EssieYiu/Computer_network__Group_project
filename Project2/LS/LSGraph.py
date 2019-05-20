922bdd1cf6a28956e3df9f23644c114ea8706f7a
EssieYiu <31929622+EssieYiu@users.noreply.github.com>
2019-05-20 15:44:52 +0800

    two node topo for D and E
    no

--- Project2/LS/LSGraph.py
+++ Project2/LS/LSGraph.py
@@ -3,8 +3,8 @@ INF = 10000000
 class LSGraph:
     def __init__(self):
         self.name_ip = {'A':"192.168.199.131",'B':"192.168.39.128","C":"",'D':"192.168.199.205",'E':"192.168.199.102"}
-        self.topo = [[0,1,INF,3,INF],[6,0,5,INF,4],[INF,1,0,INF,INF],[2,INF,INF,0,3],[INF,2,INF,5,0]]
-
+        #self.topo = [[0,1,INF,3,INF],[6,0,5,INF,4],[INF,1,0,INF,INF],[2,INF,INF,0,3],[INF,2,INF,5,0]]
+        self.topo = [[INF,INF,INF,INF,INF],[INF,INF,INF,INF,INF],[INF,INF,INF,INF,INF],[INF,INF,INF,INF,5],[INF,INF,INF,4,INF]] #only D and E exist
     def get_name_ip(self):
         return self.name_ip
 
