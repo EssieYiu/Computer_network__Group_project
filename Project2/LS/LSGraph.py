0dcf02a7e6282e30cbab7b425c89234f1901bbf6
Erout <31929622+Erout@users.noreply.github.com>
2019-05-20 16:29:47 +0800

    graph for three computers to test
    B D E

--- Project2/LS/LSGraph.py
+++ Project2/LS/LSGraph.py
@@ -4,7 +4,8 @@ class LSGraph:
     def __init__(self):
         self.name_ip = {'A':"192.168.199.131",'B':"192.168.39.128","C":"",'D':"192.168.199.205",'E':"192.168.199.102"}
         #self.topo = [[0,1,INF,3,INF],[6,0,5,INF,4],[INF,1,0,INF,INF],[2,INF,INF,0,3],[INF,2,INF,5,0]]
-        self.topo = [[INF,INF,INF,INF,INF],[INF,INF,INF,INF,INF],[INF,INF,INF,INF,INF],[INF,INF,INF,INF,5],[INF,INF,INF,4,INF]] #only D and E exist
+        #self.topo = [[INF,INF,INF,INF,INF],[INF,INF,INF,INF,INF],[INF,INF,INF,INF,INF],[INF,INF,INF,INF,5],[INF,INF,INF,4,INF]] #only D and E exist
+        self.topo = [[INF,INF,INF,INF,INF],[INF,INF,INF,4,100],[INF,INF,INF,INF,INF],[INF,5,INF,INF,3],[INF,1,INF,2,INF]] #B D E exist
     def get_name_ip(self):
         return self.name_ip
 
